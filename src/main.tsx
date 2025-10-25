94c874b5eaae602d97f94d7b1ea2a69b7afd76b9
Michael McConnell <contact@medialocal.com>
2025-10-25 12:34:28 -0700

    fix: Correct main.tsx to import ComponentLibrary instead of v3.minimal
    - Fixed main.tsx to import the main ComponentLibrary.tsx which routes to v4
    - This should now properly display all 129 components with WorkingLibraryPage
    - Deployed to https://b0a86c1e.power-components.pages.dev

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ComponentLibrary from '../ComponentLibrary.v3.minimal'
+import ComponentLibrary from '../ComponentLibrary'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
